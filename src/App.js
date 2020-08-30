65bbb02f3c99654c07f408e098cd5dcaf0958582
Archi Chester <me@qz0.ru>
2020-08-30 17:49:10 +0300

    #add component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        persons: [
+            {
+                name: "Max",
+                age: 28
+            },
+            {
+                name: "Manu",
+                age: 29
+            },
+            {
+                name: "Stephanie",
+                age: 26
+            },
+        ]
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>
+                    Hi, I'm React App!
+                </h1>
+                <p>This is really working!</p>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: React!</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+            </div>
+        );
+        // return React.createElement('div', {
+        //   className: 'App'
+        // }, React.createElement('h1', null, "I'm React App!"));
+    }
 }
 
 export default App;
